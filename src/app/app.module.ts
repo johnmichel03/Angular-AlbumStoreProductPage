cfdd923e8852f1d342dee3fa5fe7711930c93f6d
John Michel <john.michel@cityandguilds.com>
2018-03-06 22:11:06 +0000

    added pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductService } from "./product.service";
+import{NgxPaginationModule} from 'ngx-pagination';
 
 
 @NgModule({
@@ -18,7 +19,8 @@ import { ProductService } from "./product.service";
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgxPaginationModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -10,12 +10,25 @@ import { Album } from "../album";
 })
 export class ProductDescriptionComponent implements OnInit {
 albumInfo:Album;
-  constructor(private _productService: ProductService) { }
+itemsPerPage:number=5;
+page:number=1;
+pagedData=[];
+  constructor(private _productService: ProductService) {
+
+     for (var index = 0; index < 100; index++) {
+      this.pagedData.push(`Angular ${index}`);  
+    }
+   }
 
   ngOnInit() {
+   
     this._productService.getAlbum(1).subscribe(response=>
     this.albumInfo=response
     );
   }
 
+pageClicked(event){
+  this.page=event;
+console.log(event);
+}
 }
